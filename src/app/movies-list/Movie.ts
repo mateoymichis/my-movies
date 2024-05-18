a7aba4d512923e84d6bc533c071835696e8d9830
Mateo Fernandez <mateo.rost@gmail.com>
2024-05-18 16:24:50 -0300

    Agrega componente movies-list

---
+++ src/app/movies-list/Movie.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+    title: string;
+    director: string;
+    sinopsis: string;
+    duration: number;
+    year: number;
+    image: string;
+    rating: number;
+    userRating: number
+}
---
+++ src/app/movies-list/movies-list.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Movie } from './Movie';
+
+@Component({
+  selector: 'app-movies-list',
+  templateUrl: './movies-list.component.html',
+  styleUrl: './movies-list.component.scss'
+})
+export class MoviesListComponent {
+  movies: Movie[] = [
+    {
+      title: "Nueve reinas",
+      director: "Fabián Bielinsky",
+      sinopsis: "Dos estafadores intentan jugársela a un coleccionista de sellos vendiéndole una hoja de sellos raros falsificados (las \"Nueve Reinas\").",
+      duration: 115,
+      year: 2000,
+      image: "assets/img/nueve-reinas.jpg",
+      rating: 4.5,
+      userRating: 0
+    },
+    {
+      title: "El padrino",
+      director: "Francis Ford Coppola",
+      sinopsis: "El patriarca de una organización criminal transfiere el control de su clandestino imperio a su reacio hijo.",
+      duration: 175,
+      year: 1972,
+      image: "assets/img/el-padrino.jpg",
+      rating: 4.9,
+      userRating: 0
+    },
+    {
+      title: "El padrino (parte II)",
+      director: "Francis Ford Coppola",
+      sinopsis: "Relato de la vida temprana y la carrera de Vito Corleone en la ciudad de Nueva York en 1920, mientras que su hijo, Michael, se expande y refuerza su control sobre el sindicato de delitos familiares.",
+      duration: 202,
+      year: 1974,
+      image: "assets/img/el-padrino-2.jpg",
+      rating: 4.9,
+      userRating: 0
+    },
+  ];
+}
